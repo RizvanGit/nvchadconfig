00722e7a3d1828e9fa35274bdc53e3d4062d4434
RizvanGit <imeandmyep2@gmail.com>
2024-01-14 23:16:19 +0300

    feat: linting configured - lint-nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -168,6 +168,41 @@ local plugins = {
       require("smart-splits").setup()
     end,
   },
+
+  {
+   "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+        javascript = {"eslint_d"},
+        typescript = {"eslint_d"},
+        javascriptreact = {"eslint_d"},
+        typescriptreact = {"eslint_d"},
+        html = {"eslint_d"},
+        css = {"eslint_d"},
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", {
+        clear = true
+      })
+
+      vim.api.nvim_create_autocmd({"BufEnter", "BufWritePost", "InsertLeave", "ModeChanged"}, {
+        --pattern = {"*.ts", "*.js", "*.tsx", "*.jsx", "*.html", "*.css"},
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end
+      })
+
+      vim.keymap.set("n", "<leader>fl", function()
+        lint.try_lint()
+      end, {desc = "Run Linter"})
+    end
+  },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
